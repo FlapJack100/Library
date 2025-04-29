bbe9d7de47d6a8454d3a52929bb3d4e690627b36
FlapJack100 <Den_lion_17@mail.ru>
2025-04-29 10:08:26 +0300

    .

---
+++ script.js
@@ -0,0 +1,38 @@
+const openDialogBtn = document.getElementById('openDialog');
+    const bookDialog = document.getElementById('bookDialog');
+    const bookForm = document.getElementById('bookForm');
+    const bookList = document.getElementById('bookList');
+
+    openDialogBtn.addEventListener('click', () => {
+      bookDialog.showModal();
+    });
+
+    bookForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const title = document.getElementById('title').value;
+      const author = document.getElementById('author').value;
+      const pages = document.getElementById('pages').value;
+
+      addBookToLibrary(title, author, pages);
+      bookDialog.close();
+      bookForm.reset();
+    });
+
+    function addBookToLibrary(title, author, pages) {
+      const bookCard = document.createElement('div');
+      bookCard.className = 'book-card';
+
+      bookCard.innerHTML = `
+        <p class="book-title">"${title}"</p>
+        <p class="book-author">${author}</p>
+        <p class="book-pages">${pages} pages</p>
+        <button class="read-btn">Read</button>
+        <button class="remove-btn">Remove</button>
+      `;
+
+      bookCard.querySelector('.remove-btn').addEventListener('click', () => {
+        bookCard.remove();
+      });
+
+      bookList.appendChild(bookCard);
+    }
